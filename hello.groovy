4fbf572114cafa41f540be102a4d779a39c352fc
Wilson Mar <wilsonmar+github@gmail.com>
2017-06-29 17:30:38 -0400

    Create hello.groovy

---
+++ hello.groovy
@@ -0,0 +1,8 @@
+// hello.groovy in https://github.com/wilsonmar/groovy-samples
+class Example {
+   static void main(String[] args) {
+      /* This program is the first program
+      This program shows how to display hello world */
+      println('Hello World');
+   }
+}
